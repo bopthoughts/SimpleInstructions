f8341fd96075aba238d22975f6b3c8b1f7c7399d
da123rrell <darrell.darel@gmail.com>
2015-12-18 14:25:44 +0700

    test change

--- src/da123rrell/dada/MainClass.php
+++ src/da123rrell/dada/MainClass.php
@@ -23,18 +23,23 @@ class MainClass extends PluginBase{
     
     public function onCommand(CommandSender $sender,command $cmd, $label, array $args) {
 		switch($command->getName()){
-			case "buyland":
-                            $sender->sendMessage("Hello ".$sender->getName()."!");
-                            $sender->sendMessage("thank you for reading the instructions");
-							$sender->sendMessage("type /startp on the corner of your land and /endp on the other corner. after that type /landbuy");
-                            return true;
+			case "ILandbuy":
+				$sender->sendMessage("Hello ".$sender->getName()."!");
+				$sender->sendMessage("thank you for reading the instructions");
+				$sender->sendMessage("type /startp on the corner of your land and /endp on the other corner. after that type /landbuy");
+				$sender->sendMessage("use landbuy to protect your land from griefers.");
+				return true;
                         default:
-                            return false; 
-                         case "factions":
-                            $sender->sendMessage("Hello ".$sender->getName()."!");
-                            return true;
+                        	$sender->sendMessage("Acces denied");
+                        	return false;
+                         case "IFactions":
+                         	$sender->sendMessage("Hello ".$sender->getName()."!");
+                                $sender->sendMessage("Sorry this instruction is still in progress");
+                                return true;
                         default:
-                            return false;
+                        	$sender->sendMessage("Acces denied");
+                        	return false;
+                            
 		}
     }
  }
